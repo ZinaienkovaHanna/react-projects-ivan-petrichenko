97e0fc74ad96b760bd093bab713db91c34b71320
Hanna <zinaienkova.anna@gmail.com>
2023-02-27 20:47:56 +0200

    add code

--- employees-app/src/components/app-filter/app-filter.js
+++ employees-app/src/components/app-filter/app-filter.js
@@ -1,19 +1,28 @@
 import './app-filter.css';
 
-const AppFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-light" type="button">
-        Всі працівники
+const AppFilter = (props) => {
+  const buttonsData = [
+    { name: 'all', label: 'Всі працівники' },
+    { name: 'rise', label: 'На підвищення' },
+    { name: 'moreThen1000', label: 'З/П більше 1000$' },
+  ];
+
+  const buttons = buttonsData.map(({ name, label }) => {
+    const active = props.filter === name;
+    const clazz = active ? 'btn-light' : 'btn-outline-light';
+    return (
+      <button
+        key={name}
+        className={`btn ${clazz}`}
+        type="button"
+        onClick={() => props.onFilterSelect(name)}
+      >
+        {label}
       </button>
-      <button className="btn btn-outline-light" type="button">
-        На підвищення
-      </button>
-      <button className="btn btn-outline-light" type="button">
-        З/П більше 1000$
-      </button>
-    </div>
-  );
+    );
+  });
+
+  return <div className="btn-group">{buttons}</div>;
 };
 
 export default AppFilter;
